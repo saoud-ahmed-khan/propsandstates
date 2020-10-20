471af9b3c8ff46d3dca99cfc11f23a4b0a9dfe31
saoud-ahmed-khan <60979682+saoud-ahmed-khan@users.noreply.github.com>
2020-10-20 05:08:27 +0500

    ADDING FOOTER

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import './App.css';
 import {  Nav} from "./components/nav";
+import Footer, {  } from "./components/Footer";
 function App() {
   return (
     <div className="App">
       <Nav/>
+      <Footer/>
+
     </div>
   );
 }
